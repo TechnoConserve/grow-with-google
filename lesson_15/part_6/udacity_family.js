5fcffa94bafc89a6004fadebfe1aedc71147b59e
Avery Uslaner <uslaner.avery@gmail.com>
2018-01-29 15:00:34 -0700

    Array Creation
    Created simple array containing string elements.

---
+++ lesson_15/part_6/udacity_family.js
@@ -0,0 +1,8 @@
+/*
+Create an array called udaciFamily and add "Julia", "James", and your name to
+the array. Then, print the udaciFamily to the console using console.log.
+*/
+
+var udaciFamily = ["Julia", "James", "Avery"];
+
+console.log(udaciFamily);
