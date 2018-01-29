77392f7f5321183a52d13a2a7560ee86991588f9
Avery Uslaner <uslaner.avery@gmail.com>
2018-01-29 15:08:18 -0700

    Array element reassignment
    Reassigned array elements using array indexing.

---
+++ lesson_15/part_8/right_price.js
@@ -0,0 +1,12 @@
+/*
+Starting with this array of prices, change the prices of the 1st, 3rd, and 7th
+elements in the array.
+*/
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+prices[0] = 1.00;
+prices[2] = 90.00;
+prices[6] = 1.00;
+
+console.log(prices);
